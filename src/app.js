220c404bc6bf08c49a1f117d0761988610043bf9
fisble <kapilnikhilesh@gmail.com>
2026-06-03 09:38:02 +0530

    Remove nested git repository and track src files in outer repo

---
+++ src/app.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.get('/health', (req, res) => {
+    res.json({ success: true, message: 'Server is running' });
+});
+module.exports = app;
